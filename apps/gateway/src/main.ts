6875d3068d853fb5fa4d816412a587125e488eb7
Mohammad-Bilal-Bhatti <dev.bilal.bhatti@gmail.com>
2023-02-12 14:03:03 +0500

    FIXED: config service in main.ts

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,14 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
-  await app.listen(3000);
+  const port = configService.get<number>('port');
+  await app.listen(port);
 }
 bootstrap();
